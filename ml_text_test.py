aa0631f3573bfaad1c33bce020862f45fa232d3f
Lwinsan <lwinkyawwai2022@gmail.com>
2023-02-02 22:45:43 +0900

    first commit

---
+++ ml_text_test.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+from PIL import Image
+from io import BytesIO
+#from Pillow import Image
+import os
+
+# Create a folder to store the collected images
+if not os.path.exists("handwriting_images"):
+    os.makedirs("handwriting_images")
+
+st.header("မြန်မာအက္ခရာ")
+
+col1, col2 = st.columns([1,1])
+
+# Create a folder to store the collected images
+if not os.path.exists("handwriting_images"):
+    os.makedirs("handwriting_images")
+
+#text = st.text_input('Enter Alphabet!')
+
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+
+text = st.selectbox(
+        "Select Alphabet!",
+        ("00_က", "01_ခ", "02_ဂ", "03_ဃ", "04_င", "05_စ", "06_ဆ", "07_ဇ", "08_ဈ", "09_ည", "10_ဋ", "11_ဌ", "12_ဍ", "13_ဎ", "14_ဏ", "15_တ", "16_ထ", "17_ဒ", "18_ဓ", "19_န", "20_ပ", "21_ဖ", "22_ဗ", "23_ဘ", "24_မ", "25_ယ", "26_ရ", "27_လ", "28_ဝ", "29_သ", "30_ဟ", "31_ဠ", "32_အ"),
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+    )
+
+col1, col2 = st.columns([1,1])
+
+with col1:
+    canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",
+        stroke_width=13,
+        stroke_color="black",
+        background_color="white",
+        height=200,
+        width=200,
+        drawing_mode="freedraw",
+        key="canvas",
+    )
+
+
+if canvas_result.image_data is not None:
+    with col2:
+        st.image(canvas_result.image_data)
+        #submit = st.button("Save")
+        
+    im = Image.fromarray(canvas_result.image_data)
+    
+    submit = st.button("Save")
+    
+    
+    #image_counter = 0
+    
+    if submit:
+        # Increment the image counter
+        image_counter = text
+        
+        #with open(f"handwriting_images/image_{image_counter}.png", "w") as f:
+            #f.write(text)
+        
+        im.save(f"handwriting_images/Alp_{image_counter}.png")
+        
+        st.success(f"Alp_{image_counter} saved successfully!")
+        
+
+        
+        
+        
+        
